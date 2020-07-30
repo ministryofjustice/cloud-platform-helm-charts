9f32a4067fd62082c02464cac3a883ebb9dcc2fe
Imran Awan <imran.awan@digital.justice.gov.uk>
2020-07-30 10:31:07 +0100

    wrapped in s3 bucket download in function

--- report-unused-sg/files/report-stateless-vpc.go
+++ report-unused-sg/files/report-stateless-vpc.go
@@ -2,44 +2,47 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
-	"io/ioutil"
-    "os"
+	"github.com/thedevsaddam/gojsonq"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"os"
+	"log"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	
 )
 
-type Outputs struct {
-    Outputs []Output `json:"outputs"`
-}
-
-type Output struct {
-    Network Network `json:"network_id"`
-}
-
-
-type Network struct {
-    VpcID string `json:"value"`
-}
-
-
 func main() {
-	
-	jsonFile, err := os.Open("terraform.json")
+
+	DownloadFromS3Bucket("cloud-platform-terraform-state/cloud-platform-network/iawan-test/", "terraform.tfstate")
+
+	jq := gojsonq.New().File("terraform.tfstate")
+	vpcId := jq.Find("outputs.network_id.value")
+	fmt.Println(vpcId)
+}
+
+
+func DownloadFromS3Bucket(bucket string, item string) {
+	// 2) Create an AWS session
+	sess, _ := session.NewSession(&aws.Config{
+			Region: aws.String("eu-west-1")},
+	)
+
+	// 3) Create a new AWS S3 downloader 
+	downloader := s3manager.NewDownloader(sess)
+
+	// 4) Download the item from the bucket. If an error occurs, log it and exit. Otherwise, notify the user that the download succeeded.
+	file, err := os.Create(item)
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(item),
+	})
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("Unable to download item %q, %v", item, err)
 	}
 
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var outputs Outputs
-
-	json.Unmarshal(byteValue, &outputs)
-
-    for i := 0; i < len(outputs.Outputs); i++ {
-        fmt.Println("Vpc: " + outputs.Outputs[i].Network.VpcID)
-    }
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 
 }
