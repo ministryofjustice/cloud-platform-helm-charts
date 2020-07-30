5bee9636bb6a300df1cf2bc0d12e5a6cec9db637
Imran Awan <imran.awan@digital.justice.gov.uk>
2020-07-30 20:58:37 +0100

    moved logic of DescribeNatGateway into its own function

--- report-unused-sg/files/report-stateless-vpc.go
+++ report-unused-sg/files/report-stateless-vpc.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"os"
 	"log"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -14,13 +15,108 @@ import (
 
 func main() {
 
+	//Downlaod the state for 'cloud-platform-network' 
 	DownloadFromS3Bucket("cloud-platform-terraform-state/cloud-platform-network/iawan-test/", "terraform.tfstate")
 
+    //From the downloaded state file extract the VPC ID
 	jq := gojsonq.New().File("terraform.tfstate")
 	vpcId := jq.Find("outputs.network_id.value")
-	fmt.Println(vpcId)
+
+
+    //Convert the VPC ID into a string
+	var vpcIdStr string
+	vpcIdStr = fmt.Sprintf("%v", vpcId)
+	fmt.Println(vpcIdStr)
+
+	//DescribeNatGateway(vpcIdStr)
+	
+	DescribeVPC(vpcIdStr)
+
+		
 }
 
+func DescribeVPC(vpcId string) {
+
+
+		svc := ec2.New(session.New(&aws.Config{
+		Region: aws.String("eu-west-2")},))
+
+		input := &ec2.DescribeVpcsInput{
+			VpcIds: []string{
+				"vpc-0c16457fd570a1f0b",
+			},
+		}
+		
+		req := svc.DescribeVpcsRequest(input)
+		result, err := req.Send(context.Background())
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
+			}
+			return
+		}
+		
+		fmt.Println(result)
+}
+
+func DescribeNatGateway(vpcId string) {
+
+	svc := ec2.New(session.New(&aws.Config{
+		Region: aws.String("eu-west-2")},))
+	
+	input := &ec2.DescribeNatGatewaysInput{
+		Filter: []*ec2.Filter{
+			{
+				Name: aws.String("vpc-id"),
+				Values: []*string{
+					aws.String(vpcId),
+				},
+			},
+		},
+	}
+	
+	result, err := svc.DescribeNatGateways(input)
+	if err != nil {
+			fmt.Println(err.Error())
+			return
+	}
+	
+	var descNatGateways string
+	descNatGateways = fmt.Sprintf("%v", result)
+	fmt.Println(descNatGateways)
+
+	//*********************
+
+	f, err := os.Create("describeNatGateways.json")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    l, err := f.WriteString(descNatGateways)
+    if err != nil {
+        fmt.Println(err)
+        f.Close()
+        return
+	}
+	fmt.Println(l, "bytes written successfully")
+    err = f.Close()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	
+	//jq2 := gojsonq.New().File("describeNatGateways.json")
+	//natGatewayId := jq2.Find("NatGateways.NatGatewayId")
+	//fmt.Println(natGatewayId)
+
+}
 
 func DownloadFromS3Bucket(bucket string, item string) {
 	// 2) Create an AWS session
